9707524cc7b9acb509f4fcb139060ba7ec18e629
David Dionísio <49300687+deivondionisio@users.noreply.github.com>
2021-10-30 20:18:39 -0300

    adicionado dois candidatos

--- etapas.js
+++ etapas.js
@@ -2,6 +2,19 @@ let etapas = [
   {
     titulo: 'VEREADOR',
     numeros: 5,
-    candidatos: [{}]
+    candidatos: [
+      {
+        numero: 38111,
+        name: 'Fulano de Tal',
+        partido: 'ABC',
+        fotos: [{ URL: '', legenda: 'Vereador', small: false }]
+      },
+      {
+        numero: 77222,
+        name: 'Beltrano da Silva',
+        partido: 'CROK',
+        fotos: [{ URL: '', legenda: 'Vereador' }]
+      }
+    ]
   }
 ]
