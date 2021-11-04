0a4e3147f4bb5ac2d63aad098423b5f43dcbc81b
David Dionísio <49300687+deivondionisio@users.noreply.github.com>
2021-11-04 00:56:28 -0300

    finalizado a construção dos candidatos

--- etapas.js
+++ etapas.js
@@ -7,14 +7,44 @@ let etapas = [
         numero: 38111,
         name: 'Fulano de Tal',
         partido: 'ABC',
-        fotos: [{ URL: '', legenda: 'Vereador', small: false }]
+        fotos: [
+          { URL: '', legenda: 'Vereador' }
+      ]
       },
       {
         numero: 77222,
         name: 'Beltrano da Silva',
         partido: 'CROK',
-        fotos: [{ URL: '', legenda: 'Vereador' }]
-      }
+        fotos: [
+          { URL: '', legenda: 'Vereador' }
+      ]
+      },
+    ]
+  },
+  {
+    titulo: 'Prefeito',
+    numeros: 2,
+    candidatos: [
+      {
+        numero: 99,
+        name: 'Ciclano',
+        partido: 'ABC',
+        vice: 'Cic,'
+        fotos: [
+          { URL: '', legenda: 'Prefeito'},
+          { URL: '', legenda: 'Vice-Prefeito', small:true}
+      ]
+      },
+      {
+        numero: 84,
+        name: 'Zulano',
+        partido: 'QWERTY',
+        vice: 'Zul',
+        fotos: [
+          { URL: '', legenda: 'Prefeito'},
+          { URL: '', legenda: 'Vice-Prefeito', small:true}
+      ]
+      },
     ]
   }
 ]
